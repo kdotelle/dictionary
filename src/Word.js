dfbbc6e0d5b47e3ca5290353484eb0b3359d4a10
Kristen <lloyd.kristen91@gmail.com>
2022-02-11 13:10:36 -0500

    added random button

--- src/Word.js
+++ src/Word.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button } from "reactstrap";
+import { API_KEY } from "./key.js";
 import "./Word.css";
 
 function Word() {
@@ -13,7 +14,7 @@ function Word() {
     url: "https://wordsapiv1.p.rapidapi.com/words/incredible/definitions",
     headers: {
       "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "e96beec913msh200b14a276de5f0p130aaejsnf9f796cbb918",
+      "x-rapidapi-key": { API_KEY },
     },
   };
   const getWord = () => {
@@ -24,6 +25,31 @@ function Word() {
         setWord(response.data.word);
         setDefinition(response.data.definitions[0].definition);
         setPartOfSpeech(response.data.definitions[0].partOfSpeech);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const random = {
+    method: "GET",
+    url: "https://wordsapiv1.p.rapidapi.com/words/",
+    params: { random: "true" },
+    headers: {
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+      "x-rapidapi-key": { API_KEY },
+    },
+  };
+
+  const getRandom = () => {
+    axios
+      .request(random)
+      .then(function (response) {
+        console.log(response.data);
+        console.log(response.data.word);
+        // setWord(response.data.word);
+        // setDefinition(response.data.definitions[0].definition);
+        // setPartOfSpeech(response.data.definitions[0].partOfSpeech);
       })
       .catch(function (error) {
         console.error(error);
@@ -45,10 +71,19 @@ function Word() {
             block
             size="lg"
             color="secondary"
-            onClick={getWord}
+            onClick={getRandom}
             className="button"
           >
             Get Word
+          </Button>
+          <Button
+            block
+            size="lg"
+            color="primary"
+            onClick={getWord}
+            className="button"
+          >
+            Incredible
           </Button>
         </div>
       </div>
